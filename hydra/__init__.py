e146b847475ab53b2c2abbcb1cbd83a3deb18db4
Mike Wilensky <m.wilensky@qmul.ac.uk>
2022-09-15 12:05:45 +0100

    add beam_sampler to init

--- hydra/__init__.py
+++ hydra/__init__.py
@@ -1,3 +1,3 @@
 
-from . import gain_sampler, ptsrc_sampler, vis_sampler, vis_simulator, vis_utils
+from . import gain_sampler, ptsrc_sampler, vis_sampler, vis_simulator, vis_utils, beam_sampler
 from .vis_utils import *
