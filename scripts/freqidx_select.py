9c75ce27b6fcd3735933a2146dfcc13546b4ad27
Phil Bull <philbull@gmail.com>
2023-07-03 14:58:25 +0100

    Use argparse instead

--- scripts/freqidx_select.py
+++ scripts/freqidx_select.py
@@ -7,12 +7,36 @@ import numpy as np
 from pyuvdata import UVData
 import argparse, os, sys
 
-print("Down-select a data file by compressing by redundancy and selecting \n"
-      "a particular band by frequency indices.")
+import argparse, os, sys, time
+import pyuvsim
 
-fname = str(sys.argv[1])
-idx_min = int(sys.argv[2])
-idx_max = int(sys.argv[3])
+#sys.path.insert(0,'/home/phil/hera/Hydra/')
+sys.path.insert(0,'/cosma/home/dp270/dc-bull2/software/Hydra/')
+import hydra
+
+# Set up argparser
+description = "Down-select a data file by compressing by redundancy and selecting a particular band by frequency indices."
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument("--filename", type=str, action="store", 
+                    required=True, dest="fname",
+                    help="Path to input UVData file.")
+parser.add_argument("--idx-min", type=int, action="store",
+                    required=True, dest="idx_min",
+                    help="Index of lower bound of frequency range.")
+parser.add_argument("--idx-max", type=int, action="store",
+                    required=True, dest="idx_max",
+                    help="Index of upper bound of frequency range.")
+parser.add_argument("--compress", type=str, action="store", default="none", 
+                    required=False, dest="compress",
+                    help="Compress by redundancy. Options are 'none', 'select', or 'average'.")
+args = parser.parse_args()
+
+# Extract args
+fname = arg.fname
+idx_min = arg.idx_min
+idx_max = arg.idx_max
+compress = arg.compress
+assert compress in ['none', 'select', 'average']
 
 print("Input file:", fname)
 print("Freq. indices between %4d -- %4d inclusive" % (idx_min, idx_max))
@@ -20,7 +44,8 @@ print("Freq. indices between %4d -- %4d inclusive" % (idx_min, idx_max))
 # Load full data file and select down by redundancy
 uvd = UVData()
 uvd.read_uvh5(fname)
-uvd.compress_by_redundancy(method='select', inplace=True)
+if compress != 'none':
+    uvd.compress_by_redundancy(method=compress, inplace=True)
 
 # Frequency selection
 freqs = np.unique(uvd.freq_array)
