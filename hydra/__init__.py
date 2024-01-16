75b4197e708e9817a00eabb5b21b0ea9dd372ee9
Mike Wilensky <m.wilensky@qmul.ac.uk>
2024-01-16 13:54:26 +0000

    Add sparse_beam to init

--- hydra/__init__.py
+++ hydra/__init__.py
@@ -1,4 +1,4 @@
 
-from . import beam_sampler, diffuse_sampler, gain_sampler, ptsrc_sampler, vis_sampler, \
-              vis_simulator, utils
+from . import beam_sampler, diffuse_sampler, gain_sampler, ptsrc_sampler, \
+              sparse_beam, vis_sampler, vis_simulator, utils
 from .utils import *
